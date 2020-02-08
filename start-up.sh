c2b6f6eb50efec8643d58c920ea9b738c796c780
e136063 <joseph.decker@wnco.com>
2020-02-07 23:17:20 -0600

    Joseph - conditional test for openssl file

--- start-up.sh
+++ start-up.sh
@@ -12,12 +12,15 @@ cp nginx/nginx.initial.conf nginx/nginx.conf
 docker-compose up -d
 
 
-sudo openssl dhparam -out dhparam/dhparam-2048.pem 2048
+[ -f dhparam/dhparam-2048.pem ] && echo "Found openssl file. Skipping" || echo "OpenSSL not found. Creating..." && sudo openssl dhparam -out dhparam/dhparam-2048.pem 2048
 
+echo "sleeping for 5"
 sleep 5
 
 docker-compose stop nginx
 
+echo "sleeping for 5"
+sleep 5
 
 rm nginx/nginx.conf 
 cp nginx/nginx.final.conf nginx/nginx.conf
